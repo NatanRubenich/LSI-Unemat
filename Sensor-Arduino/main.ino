907f92609ddd4bafa50bcc0a238c13d811482df0
NatanRubenich <natan_rubenich@icloud.com>
2021-02-02 16:06:39 -0300

    Update main.ino

--- Sensor-Arduino/main.ino
+++ Sensor-Arduino/main.ino
@@ -1,9 +1,82 @@
-// Sensor Cor Rgb E Gesto Apds-9960 Esp8266 Arduino
+
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+
+// Pins
+#define APDS9960_INT    2 // Needs to be an interrupt pin
+
+// Constants
+
+// Global Variables
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+int isr_flag = 0;
 
 void setup() {
+
+  // Set interrupt pin as input
+  pinMode(APDS9960_INT, INPUT);
+
+  // Initialize Serial port
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println(F("--------------------------------"));
+  Serial.println(F("SparkFun APDS-9960 - GestureTest"));
+  Serial.println(F("--------------------------------"));
   
+  // Initialize interrupt service routine
+  attachInterrupt(0, interruptRoutine, FALLING);
+
+  // Initialize APDS-9960 (configure I2C and initial values)
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+  
+  // Start running the APDS-9960 gesture sensor engine
+  if ( apds.enableGestureSensor(true) ) {
+    Serial.println(F("Gesture sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during gesture sensor init!"));
+  }
 }
 
 void loop() {
+  if( isr_flag == 1 ) {
+    detachInterrupt(0);
+    handleGesture();
+    isr_flag = 0;
+    attachInterrupt(0, interruptRoutine, FALLING);
+  }
+}
 
+void interruptRoutine() {
+  isr_flag = 1;
+}
+
+void handleGesture() {
+    if ( apds.isGestureAvailable() ) {
+    switch ( apds.readGesture() ) {
+      case DIR_UP:
+        Serial.println("UP");
+        break;
+      case DIR_DOWN:
+        Serial.println("DOWN");
+        break;
+      case DIR_LEFT:
+        Serial.println("LEFT");
+        break;
+      case DIR_RIGHT:
+        Serial.println("RIGHT");
+        break;
+      case DIR_NEAR:
+        Serial.println("NEAR");
+        break;
+      case DIR_FAR:
+        Serial.println("FAR");
+        break;
+      default:
+        Serial.println("NONE");
+    }
+  }
 }
